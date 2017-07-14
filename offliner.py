1353c1b802136f4bcd36320da4212266c66e1faa
Ondřej Slabý <kron258@gmail.com>
2017-07-14 21:03:32 +0200

    Offliner plugin now counts days for which a user has been offline while the bot is running. When the user comes online again, their offline day count is saved.

--- offliner.py
+++ offliner.py
@@ -2,18 +2,28 @@
 
 from disco.bot import Plugin
 from disco.types.user import Status
-from time import time
+from gevent import sleep, kill, GreenletExit
 
 class Offliner(Plugin):
     """
     Plugin to count the days a person has been offline, along with
     statistics for comparison.
     """
+    DAY = 24 * 3600
 
     def load(self, ctx):
         super().load(ctx)
-        self.stats = self.storage.guild('offliner_stats')
+
+        self.maximum = self.storage.guild('offliner_maximum')
+        self.history = self.storage.guild('offliner_history')
         self.current = {}
+        self.counters = {}
+
+    def unload(self, ctx):
+        super().unload(ctx)
+
+        for counter in self.counters.values():
+            counter.kill()
 
     @Plugin.command('ping')
     def command_ping(self, event):
@@ -21,6 +31,28 @@ class Offliner(Plugin):
 
     @Plugin.listen('PresenceUpdate')
     def member_presence_update(self, event):
+        userid = str(event.user.id)
+
         if event.status == Status.OFFLINE:
-            self.stats[event.user.id] = time()
-            
+            self.current[userid] = 0
+            def counter():
+                while True:
+                    sleep(DAY)
+                    self.current[userid] += 1
+
+            self.counters[userid] = self.spawn(counter)
+        elif userid in self.counters:
+            self.counters[userid].kill()
+            del self.counters[userid]
+
+            # Save maximum and statistics
+            try:
+                if self.maximum[userid] < self.current[userid]:
+                    self.maximum[userid] = self.current[userid]
+            except KeyError:
+                self.maximum[userid] = self.current[userid]
+
+            try:
+                self.history[userid] = self.history[userid] + [self.current[userid]]
+            except KeyError:
+                self.history[userid] = [self.current[userid]]
