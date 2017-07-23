3c4541d8f5b6d9be02594f594d621efb1a8ef6f2
Ondřej Slabý <kron258@gmail.com>
2017-07-23 13:21:33 +0200

    Added active subreddit listing.

--- redditnews.py
+++ redditnews.py
@@ -8,13 +8,28 @@ class RedditNews(Plugin):
     the results which are new to the specified channel.
     """
 
+
     def load(self, ctx):
         super().load(ctx)
 
         self.settings = self.storage.guild('redditnews_settings')
         self.searches = self.storage.guild('redditnews_searches')
 
+
     @Plugin.command('redditnews here', aliases=['rdnh', 'rdn here'])
     def redditnews_channel(self, event):
         self.settings['channel'] = event.channel.id
         event.channel.send_message('I will now post my Reddit news here.')
+
+
+    @Plugin.command('redditnews list', aliases=['rdnl', 'rdn list'])
+    def redditnews_list(self, event):
+        if len(self.searches):
+            msg = 'Currently set up subreddits: '
+        else:
+            msg = 'There are currently no set up subreddits.'
+
+        for k in self.searches.keys():
+            msg += k
+
+        event.msg.reply(msg)
