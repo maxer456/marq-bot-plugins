08496757b9622e9bea890d76be230de2f48382ea
Ondřej Slabý <kron258@gmail.com>
2019-12-23 22:46:18 +0100

    Final changes, might not work, unmaintained. Might rewrite.

--- offliner.py
+++ offliner.py
@@ -12,7 +12,6 @@ class Offliner(Plugin):
     Plugin to count the days a person has been offline, along with
     statistics for comparison.
     """
-    DAY = 24 * 3600
 
 
     def load(self, ctx):
@@ -22,114 +21,70 @@ class Offliner(Plugin):
         self.history = self.storage.guild('offliner_history')
         self.status = self.storage.guild('offliner_status')
 
-        self.current = {}
-        self.counters = {}
-
 
     def unload(self, ctx):
         super().unload(ctx)
-
-        for counter in self.counters.values():
-            counter.kill()
-
-
-    @Plugin.listen('GuildCreate')
-    def guild_create(self, event):
-        self.current[event.guild.id] = {}
-        self.counters[event.guild.id] = {}
-
-        for userid, val in self.status.items():
-            at, points = val
-            delay = self.DAY - (datetime.now() - datetime.fromtimestamp(at)).seconds
-            self.start_counter(userid, delay, points)
-
-
-    @Plugin.listen('GuildMembersChunk')
-    def members_chunk(self, event):
-        counters = self.counters[event.guild_id]
-        for member in event.members:
-            userid = str(member.user.id)
-
-            if (userid in counters and
-                    member.user.presence != UNSET and
-                    member.user.presence != Status.OFFLINE):
-                self.stop_counter(userid)
 
 
     @Plugin.listen('PresenceUpdate')
     def member_presence_update(self, event):
         userid = str(event.user.id)
-        counters = self.counters[event.guild_id]
+        time = datetime.now()
+        timestamp = time.hour * 3600 + time.minute * 60 + time.second
+        timestamp //= 10
+        timestamp %= 10
 
         if event.status == Status.OFFLINE:
-            if userid in counters:
-                # Should not happen
-                counters[userid].kill()
-                del counters[userid]
+            try:
+                status = self.status[timestamp]
+            except KeyError:
+                status = {}
 
-            self.status[userid] = datetime.now().timestamp(), 0
-            self.start_counter(userid)
+            status[userid] = 0
+            print(timestamp)
+            self.status[timestamp] = status
+        else:
+            try:
+                status = self.status[timestamp]
+                del status[userid]
+                self.status[timestamp] = status
+            except KeyError:
+                pass
 
-        elif userid in counters:
-            self.stop_counter(userid)
+
+    @Plugin.schedule(10, repeat=True, init=False)
+    def offliner_schedule(self):
+        time = datetime.now()
+        timestamp = time.hour * 3600 + time.minute * 60 + time.second
+        timestamp //= 10
+        timestamp %= 10
+        print(timestamp)
+        for guild in self.state.guilds.values():
+            self.offliner_guild_check(guild, timestamp)
 
 
-    def start_counter(self, userid, presleep=None, points=0):
-        guild = self.ctx['guild']
+    def offliner_guild_check(self, guild, stamp):
+        self.ctx['guild'] = guild
+
+        try:
+            status = self.status[stamp]
+            newstatus = {}
+
+            for uid, val in status.items():
+                if uid in self.state.users:
+                    user = self.state.users[uid]
+                    if user.presence == UNSET or user.presence == Status.OFFLINE:
+                        newstatus[uid] = status[uid] + 1
+                        self.send_off_line(guild, user, newstatus[uid])
+
+            self.status[stamp] = newstatus
+        except KeyError:
+            pass
+        finally:
+            self.ctx.drop()
+
+
+    def send_off_line(self, guild, user, lines):
         channel = next(iter(guild.channels.values()))
-        current = self.current[guild.id]
-        counters = self.counters[guild.id]
-
-        current[userid] = points
-
-        def counter():
-            preslept = False
-            while True:
-                if presleep and not preslept:
-                    sleep(presleep)
-                    preslept = True
-                else:
-                    sleep(self.DAY)
-
-                current[userid] += 1
-                points = '~~||||~~ ' * (current[userid] // 5) + '|' * (current[userid] % 5)
-                channel.send_message('<@{}>: {}'.format(userid, points))
-
-                # Save current points in case of a sudden shutdown
-                self.ctx['guild'] = guild
-                try:
-                    at, _ = self.status[userid]
-                    self.status[userid] = at, current[userid]
-                finally:
-                    self.ctx.drop()
-
-        counters[userid] = self.spawn(counter)
-
-
-    def stop_counter(self, userid):
-        guildid = self.ctx['guild'].id
-        current = self.current[guildid]
-        counters = self.counters[guildid]
-
-        counters[userid].kill()
-        del counters[userid]
-        del self.status[userid]
-
-        # Do not save zeroes
-        if current[userid] == 0:
-            del current[userid]
-            return
-
-        # Save maximum and statistics
-        try:
-            if self.maximum[userid] < current[userid]:
-                self.maximum[userid] = current[userid]
-        except KeyError:
-            self.maximum[userid] = current[userid]
-
-        try:
-            self.history[userid] = self.history[userid] + [current[userid]]
-        except KeyError:
-            self.history[userid] = [current[userid]]
-
-        del current[userid]
+        points = '~~||||~~ ' * (lines // 5) + '|' * (lines % 5)
+        channel.send_message('<@{}>: {}'.format(user.id, points))
